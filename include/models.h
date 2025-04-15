661bfa2e5967a918b82c20347f126d9c97a68008
ThatHypedPerson <thathypedperson@gmail.com>
2025-04-14 22:00:07 -0700

    simplify human link recolor

---
+++ include/models.h
@@ -0,0 +1,17 @@
+#include "global.h"
+
+// generic
+extern Gfx* gPlayerWaistDLs[2 * PLAYER_FORM_MAX];
+extern Gfx* sPlayerFirstPersonRightShoulderDLs[PLAYER_FORM_MAX];
+
+// human link
+extern Gfx gLinkHumanWaistModifiedDL;
+extern Gfx gLinkHumanRightThighModifiedDL;
+extern Gfx gLinkHumanLeftThighModifiedDL;
+extern Gfx gLinkHumanHeadModifiedDL;
+extern Gfx gLinkHumanHatModifiedDL;
+extern Gfx gLinkHumanCollarModifiedDL;
+extern Gfx gLinkHumanLeftShoulderModifiedDL;
+extern Gfx gLinkHumanRightShoulderModifiedDL;
+extern Gfx gLinkHumanTorsoModifiedDL;
+extern FlexSkeletonHeader gLinkHumanSkelMod;
